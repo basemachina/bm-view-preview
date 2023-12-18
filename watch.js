22d9a865e076003192e367d764a0325ce21c942d
yebis0942 <kento.fukuda@basemachina.com>
2023-12-18 18:47:32 +0900

    add comments

--- watch.js
+++ watch.js
@@ -1,7 +1,10 @@
 const fs = require("node:fs/promises");
 
+/** ファイルが変更されるごとにファイルの内容を返すAsyncGenerator */
 async function* watch(filePath) {
   const read = () => fs.readFile(filePath, { encoding: "utf8" });
+
+  // 初回は更新されなくても内容を読んで返す
   yield read();
 
   const watcher = fs.watch(filePath);
